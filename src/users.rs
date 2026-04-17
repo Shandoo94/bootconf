0fe014cffdd116d14fef6ed998d44dca665e274a
Shandoo <gitmail1.scorch808@passmail.net>
2026-04-17 09:29:22 +0200

    style: reimplement no defaults for useradd

--- src/users.rs
+++ src/users.rs
@@ -1,18 +1,13 @@
 use nix::unistd;
 use serde::Deserialize;
-use std::error::Error;
 use std::fs;
-use std::io;
 use std::path;
 use std::process;
-use std::str::FromStr;
 
 pub const DEFAULT_ROOT: &str = "/";
 pub const PASSWD_PATH: &str = "etc/passwd";
 pub const SHADOW_PATH: &str = "etc/shadow";
 pub const GROUP_PATH: &str = "etc/group";
-pub const DEFAULT_SHELL: &str = "/bin/sh";
-pub const DEFAULT_HOME_PREFIX: &str = "/home";
 pub const SSH_DIR: &str = ".ssh";
 pub const AUTHORIZED_KEYS: &str = "authorized_keys";
 pub const HOME_DIR_MODE: u32 = 0o700;
@@ -56,7 +51,7 @@ pub fn apply_user(
     let root_path = root.unwrap_or(path::Path::new(DEFAULT_ROOT));
 
     match unistd::User::from_name(&user.name).ok().flatten() {
-        Some(existing_user) => modify_user(user, &existing_user, root_path)?,
+        Some(existing_user) => modify_user(user, &existing_user, root_path),
         None => create_user(user, root_path)?,
     };
 
@@ -65,44 +60,33 @@ pub fn apply_user(
     Ok(())
 }
 
-pub fn create_user(user: &User, root: &path::Path) -> Result<()> {
+pub fn create_user(user: &User, root: &path::Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = process::Command::new("useradd");
-
-    let shell_path: path::PathBuf = root.join(path::Path::new(DEFAULT_SHELL));
-    let home_dir = if let Some(home_dir) = user.home {
-        root.join(home_dir)
-    } else {
-        root.join(path::Path::new(DEFAULT_HOME_PREFIX))
-            .join(user.name)
-    };
 
     cmd.arg("-m")
         .arg("-U")
-        .arg("-s")
-        .arg(shell_path.to_string_lossy())
         .arg("-u")
         .arg(user.uid.to_string())
         .arg("R")
-        .arg(root.to_string_lossy())
-        .arg("-d")
-        .arg(home_dir.to_string_lossy());
+        .arg(root.to_string_lossy().to_string());
 
-    if let Some(groups) = user.groups {
+    if let Some(shell) = &user.shell {
+        cmd.arg("-s").arg(shell);
+    };
+
+    if let Some(home_dir) = &user.home {
+        cmd.arg("-d").arg(root.join(home_dir));
+    };
+
+    if let Some(groups) = &user.groups {
         cmd.arg("-G").arg(groups.join(","));
     };
 
-    cmd.arg(user.name);
+    cmd.arg(&user.name);
 
-    let status = cmd.status()?;
+    let _ = cmd.status();
 
-    if status.success() {
-        Ok(())
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            format!("Command 'useradd' failed with exit status: {}", status),
-        ))
-    }
+    Ok(())
 }
 
 pub fn modify_user(user: &User, existing_user: &unistd::User, root: &path::Path) {}
